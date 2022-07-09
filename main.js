903ed6bab601a65f9f9304b266ed01f00f024b2d
Braxton <65457602+braxttsl-dev@users.noreply.github.com>
2022-07-09 15:16:32 -0400

    update 7/9

--- main.js
+++ main.js
@@ -1,42 +1,19 @@
 const profile = document.querySelector(".card__profile")
 const share = document.querySelector(".share-modal");
-const popUpShare = document.querySelector(".pop-up")
 const shareBtnActive = document.querySelector("#shareBtnActive")
 const shareBtnUnactive = document.querySelector("#shareBtnUnactive")
 
 
 shareBtnUnactive.addEventListener("click", () => {
-    if (window.innerWidth < 767) {
-        profile.style.display = "none"
-        share.style.display = "flex"
-        popUpShare.style.display = "none"
-    }
+   share.classList.toggle("flex")
 })
+
 
 shareBtnActive.addEventListener("click", () => {
-    if (window.innerWidth < 767) {
-        profile.style.display = "flex"
-        share.style.display = "none"
-        popUpShare.style.display = "none"
-    }
+    share.classList.remove("flex")
+
+   
 })
 
 
-shareBtnUnactive.addEventListener("click", () => {
-    if (window.innerWidth > 767) {
-        if (popUpShare.style.display === "none") {
-            popUpShare.style.display = "flex";
-            share.style.display = "none"
-        } else {
-            popUpShare.style.display = "none";
-            share.style.display = "none"
-        }
-    }
-})
-
-
-
-
-//  if (window.innerWidth > 767)
-
-// It seems that the "share" element's button is a different button
+// Main issue the button
